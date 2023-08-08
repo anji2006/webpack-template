39a97537e7003be583bac89d35d8b81dc361ccd0
anji <aveeranki@nouveau-labs.com>
2023-08-08 22:16:26 +0530

    auth HOC

--- src/HOC/Authontication.js
+++ src/HOC/Authontication.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Auth = WrapperComponent => props => {
   const token = localStorage.getItem('token')
-  if (!token) return <WrapperComponent {...props} />
+  if (token) return <WrapperComponent {...props} />
   return <div> this login Page</div>
 }
 
